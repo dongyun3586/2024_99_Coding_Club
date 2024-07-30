f0c287fc3a349502f46f872ac75db0b340d15bb3
leedong <goodluck3586@gmail.com>
2024-07-30 13:33:39 +0900

    Update ex09_최소 힙.py

--- Level 3 챌린저/ex09_최소 힙.py
+++ Level 3 챌린저/ex09_최소 힙.py
@@ -1,2 +1,18 @@
 # https://www.acmicpc.net/problem/1927
+import sys
+from heapq import heappush, heappop
 
+input = sys.stdin.readline
+
+n = int(input())
+A = []
+
+for _ in range(n):
+    v = int(input())
+    if v == 0:
+        if A:
+            print(heappop(A))
+        else:
+            print(v)
+    else:
+        heappush(A, v)
