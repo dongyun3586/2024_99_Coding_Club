7f18e55ded347a63fedbb610a278e089be9690eb
leedong <goodluck3586@gmail.com>
2024-08-31 12:32:36 +0900

    gg

---
+++ Level 2 미들러/ex35_게임 맵 최단거리_BFS1.py
@@ -0,0 +1,40 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+from collections import deque
+
+
+def solution(maps):
+    n, m = len(maps), len(maps[0])  # 맵의 크기 저장
+
+    # BFS를 위한 큐 초기화
+    queue = deque([(0, 0, 1)])  # (행, 열, 거리)
+    visited = [[False] * m for _ in range(n)]
+    visited[0][0] = True  # 시작지점 방문 처리
+
+    while queue:
+        x, y, d = queue.popleft()
+
+        # 목적지에 도착한 경우 이동 거리 반환
+        if x == n - 1 and y == m - 1:
+            return d
+
+        # 네 방향으로 이동
+        for dx, dy in [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]:
+            if 0 <= dx < n and 0 <= dy < m and not visited[dx][dy] and maps[dx][dy]:
+                visited[dx][dy] = True
+                queue.append((dx, dy, d + 1))
+
+    return -1
+
+
+if __name__ == '__main__':
+    print(solution([[1, 0, 1, 1, 1],
+                    [1, 0, 1, 0, 1],
+                    [1, 0, 1, 1, 1],
+                    [1, 1, 1, 0, 1],
+                    [0, 0, 0, 0, 1]]))  # 11
+
+    print(solution([[1, 0, 1, 1, 1],
+                    [1, 0, 1, 0, 1],
+                    [1, 0, 1, 1, 1],
+                    [1, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 1]]))  # -1
