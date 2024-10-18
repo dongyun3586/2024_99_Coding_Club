d9c41a9caa5b8afa575f9fbef54d94d8e0fc4b35
leedong <goodluck3586@gmail.com>
2024-10-18 18:20:16 +0900

    gg
    ggg

--- Level 2 미들러/ex09_더 맵게.py
+++ Level 2 미들러/ex09_더 맵게.py
@@ -2,8 +2,9 @@
 
 from heapq import heapify, heappush, heappop
 
+
 def solution(scoville, K):
-    heapify(scoville)   # scoville 리스트를 힙으로 변환
+    heapify(scoville)  # scoville 리스트를 힙으로 변환
     count = 0
     while len(scoville) > 1:
         min1, min2 = heappop(scoville), heappop(scoville)
@@ -17,4 +18,4 @@ def solution(scoville, K):
 
 
 if __name__ == "__main__":
-    print(solution([1, 2, 3, 9, 10, 12],7))     # 2
+    print(solution([1, 2, 3, 9, 10, 12], 7))  # 2
--- Level 2 미들러/ex20_큰 수 만들기.py
+++ Level 2 미들러/ex20_큰 수 만들기.py
@@ -1,21 +1,24 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42883
 
 def solution(number, k):
-    target_length = len(number) - k
-    number = [i for i in number]
-    stack = []
-    for i in number:
-        while stack and i > stack[-1] and k:
+    stack = []  # 결과를 저장할 스택
+    for num in number:
+        # 스택의 마지막 값이 현재 값보다 작으면 스택에서 제거 (k번 수행 가능)
+        while k and stack and stack[-1] < num:
             stack.pop()
             k -= 1
-        stack.append(i)
-    while len(stack) > target_length:
-         stack.pop()
+        stack.append(num)
+
+    # 만약 아직 제거할 숫자가 남았다면, 스택의 뒤에서부터 제거
+    if k > 0:
+        stack = stack[:-k]
+
+    # 스택에 남아있는 숫자들을 합쳐서 반환
     return ''.join(stack)
 
 
 if __name__ == "__main__":
-    print(solution("1924", 2))      # "94
-    print(solution("1231234",3))    # "3234"
-    print(solution("4177252841",4)) # "775841"
-    print(solution("999", 1))       # 99
+    print(solution("1924", 2))  # "94
+    print(solution("1231234", 3))  # "3234"
+    print(solution("4177252841", 4))  # "775841"
+    print(solution("999", 1))  # 99
